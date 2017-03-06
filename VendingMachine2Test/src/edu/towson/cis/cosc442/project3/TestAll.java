626b46a4fad5606eec46896d886159811874e956
russellgehret <rgehre1@students.towson.edu>
2017-03-05 21:26:50 -0500

    Task 2
    Generating Unit Tests

---
+++ VendingMachine2Test/src/edu/towson/cis/cosc442/project3/TestAll.java
@@ -0,0 +1,32 @@
+package edu.towson.cis.cosc442.project3;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * The class <code>TestAll</code> builds a suite that can be used to run all
+ * of the tests within its package as well as within any subpackages of its
+ * package.
+ *
+ * @generatedBy CodePro at 3/5/17 8:29 PM
+ * @author Russell
+ * @version $Revision: 1.0 $
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	edu.towson.cis.cosc442.project3.vendingmachine.TestAll.class,
+})
+public class TestAll {
+
+	/**
+	 * Launch the test.
+	 *
+	 * @param args the command line arguments
+	 *
+	 * @generatedBy CodePro at 3/5/17 8:29 PM
+	 */
+	public static void main(String[] args) {
+		JUnitCore.runClasses(new Class[] { TestAll.class });
+	}
+}
